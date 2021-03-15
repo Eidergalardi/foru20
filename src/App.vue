dfd18f9e83761eed0efdbd1c9a38410580ecddb9
karol.djanashvili <karol.djanashvili@fib.upc.edu>
2021-03-15 23:01:56 +0100

    1. añadir form estructura 2. tabla para poner listados de foros 3. barra superior para el menu 4. conexion BD 5. conexion firebase

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,101 @@
 <template>
-
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <HelloWorld></HelloWorld>
+    <nav class="navbar navbar-dark bg-primary">
+      <a href="/" class="navbar-brand">Teacher's Forum Community</a>
+    </nav>
+    <!-- main content -->
+    <div class="container">
+      <div class="row mt-5">
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-header">
+              <h3>Add a new forum</h3>
+            </div>
+            <div class="card-body">
+              <form>
+                <div class="form-group">
+                  <label>
+                    <input type="text" class="font-control" v-model="newForum.name" placeholder="Name">
+                  </label>
+                  <label>
+                    <input type="text" class="font-control" v-model="newForum.author" placeholder="Author">
+                  </label>
+                  <label>
+                    <input type="text" class="font-control" v-model="newForum.url" placeholder="Url">
+                  </label>
+
+                  <br><br>
+                  <button type="submit" class="btn btn-primary">
+                    Save
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-8 text-center">
+          <img src="./assets/logo.png" alt="">
+          <div class="card">
+            <div class="card-header">
+              <h3>Forums Lists</h3>
+            </div>
+            <div class="card-body">
+              <table class="table table-striped table-bordered">
+                <thead>
+                  <tr>
+                    <th>Name</th>
+                    <th>Autor</th>
+                    <th>Message</th>
+                  </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//modulo para conectar a bd real
+import Firebase from 'firebase';
+import config from './config';
+
+//app contiene conexion de firebase
+let app = Firebase.initializeApp(config);
+
+//db contiene conexion a la para usar el servicio
+let db = app.database();
+let newForumRef = db.ref('websites');
+//let authentication = app.auth();
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  firebase: {
+    forums: newForumRef,
+  },
+  data(){
+    return {
+      newForum: {
+        name: '',
+        author: '',
+        url: '',
+      }
+    }
 
+  },
+
+}
 </script>
 
 <style>
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/config.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyDVhgGOFS60P_Dc1dpEFxphnAB_MM7jtPM",
+    authDomain: "foru20.firebaseapp.com",
+    databaseURL: "https://foru20-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "foru20",
+    storageBucket: "foru20.appspot.com",
+    messagingSenderId: "1075618869981",
+    appId: "1:1075618869981:web:df212572727bc6fc44a134",
+    measurementId: "G-0NLT180ZJK"
+}
